5cb20e5555d19a5f028bdcd6ed6301fee22510ef
Joel0612 <https://github.com/Joel0612/Venta.git>
2025-10-16 02:09:12 -0400

    Corregida ruta de db.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const { conectarDB, Mensaje } = require("./express/db/db");
+const { conectarDB, Mensaje } = require("./db.js");
 
 const app = express();
 app.use(cors());
